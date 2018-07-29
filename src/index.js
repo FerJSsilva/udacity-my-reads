5929778bf6a9abbf77f07827f0bfb0a997c8ea26
Fernando <development.ferjssilva@gmail.com>
2018-07-29 20:21:02 -0300

    feat: add udacity my read start files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/App';
-
-import './assets/styles/style.sass';
-import './assets/styles/style.css';
+import './assets/styles/index.css';
 
 ReactDOM.render(
   React.createElement(App),
