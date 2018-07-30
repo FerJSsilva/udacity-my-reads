130cc77340cd5c33c30b5ab73f82b5088f217c51
Fernando <development.ferjssilva@gmail.com>
2018-07-29 22:57:29 -0300

    feat: configure App to fetch data from api and pass it as props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import * as BooksAPI from '../utils/BooksAPI';
+import * as BooksAPI from '../utils/BooksAPI';
 
 import Booklist from './pages/BookList';
 import Booksearch from './pages/BookSearch';
@@ -9,18 +9,30 @@ import Booksearch from './pages/BookSearch';
 import './App.css';
 
 export class App extends Component {
-  componentDidMount() {
-    // fetch will go here
+  state = {
+    myBooks: [],
   }
 
+  componentDidMount() {
+    this.fetchBookList();
+  }
+
+  fetchBookList = () => {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState({ myBooks: books });
+      });
+  };
+
   render() {
+    const { myBooks } = this.state;
     return (
       <BrowserRouter>
         <div className="app">
           <div>
             <Switch>
-              <Route exact path="/" component={Booklist} />
-              <Route path="/search" component={Booksearch} />
+              <Route exact path="/" render={() => <Booklist myBooks={myBooks} />} />
+              <Route path="/search" render={() => <Booksearch myBooks={myBooks} />} />
             </Switch>
           </div>
         </div>
