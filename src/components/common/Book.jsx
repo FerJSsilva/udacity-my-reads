75aea84686f13aec0394d6c0538593dd51487400
Fernando <development.ferjssilva@gmail.com>
2018-08-01 05:54:17 -0300

    feat: fix book logic to check for null thumb or shelf

--- src/components/common/Book.jsx
+++ src/components/common/Book.jsx
@@ -25,11 +25,11 @@ class Book extends PureComponent {
               style={{
                 width: 128,
                 height: 193,
-                backgroundImage: `url("${book.imageLinks.thumbnail}")`,
+                backgroundImage: `url("${book.imageLinks && book.imageLinks.thumbnail}")`,
               }}
             />
             <div className="book-shelf-changer">
-              <select onChange={this.changeShelf} value={book.shelf}>
+              <select onChange={this.changeShelf} value={book.shelf || 'none'}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
