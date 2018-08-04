f72279a6b635c88b0c4451c3612c81f053ee8cc1
Fernando <development.ferjssilva@gmail.com>
2018-08-04 15:14:30 -0300

    fix: remove the value of disabled option

--- src/components/common/Book.jsx
+++ src/components/common/Book.jsx
@@ -30,7 +30,7 @@ class Book extends PureComponent {
             />
             <div className="book-shelf-changer">
               <select onChange={this.changeShelf} value={book.shelf || 'none'}>
-                <option value="none" disabled>Move to...</option>
+                <option disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
